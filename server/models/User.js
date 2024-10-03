e0694e424a447fb915bdfd95e945fbaf04db3147
medriadi <mohamedriadi@outlook.com>
2024-10-03 16:05:38 +0100

    Define User model with schema for MongoDB

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  scores: {
+    type: [Number],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
