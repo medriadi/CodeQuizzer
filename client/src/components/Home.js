6fa551b2c52caa059f689b0f8318e60d116b9aef
medriadi <mohamedriadi@outlook.com>
2024-10-14 22:06:30 +0100

    Refactor Home component to include a styled jumbotron with a "Get Started" button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="text-center">
-      <h1>Welcome to CodeQuizzer</h1>
-      <p>Your ultimate platform to test and improve your programming knowledge!</p>
+    <div className="text-center p-5">
+      <div className="jumbotron bg-light shadow-sm p-4 rounded">
+        <h1 className="display-4">Welcome to CodeQuizzer</h1>
+        <p className="lead">Your ultimate platform to test and improve your programming knowledge!</p>
+        <hr className="my-4" />
+        <Link className="btn btn-primary btn-lg" to="/quizzes" role="button">Get Started</Link>
+      </div>
     </div>
   );
 };
