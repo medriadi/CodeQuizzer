ab27999eda8d901c6c8410f3795751fe8cac65fc
medriadi <mohamedriadi@outlook.com>
2024-10-14 22:07:15 +0100

    Refactor ProtectedRoute component to display a spinner while loading

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -6,7 +6,13 @@ const ProtectedRoute = ({ children }) => {
   const { auth } = useContext(AuthContext);
 
   if (auth.loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="text-center mt-4">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
   }
 
   return auth.isAuthenticated ? children : <Navigate to="/login" />;
