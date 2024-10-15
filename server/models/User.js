a949099b240bfddf53d9bbea725aa3ab4daf38b5
medriadi <mohamedriadi@outlook.com>
2024-10-15 19:32:06 +0100

    Refactor User model to include password hashing and validation for username and email

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const QuizAttemptSchema = new mongoose.Schema({
   quizId: {
@@ -25,21 +27,41 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 30,
+    validate: [validator.isAlphanumeric, 'Username must be alphanumeric'],
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Invalid email address'],
   },
   password: {
     type: String,
     required: true,
+    minlength: 6,
   },
   quizAttempts: [QuizAttemptSchema],
   date: {
     type: Date,
     default: Date.now,
   },
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
